e1a15caa16115e9a6f2a26eecf68af11b9d7e38d
1102909182 <1102909182@qq.com>
2020-08-18 14:10:07 +0800

    first commit

---
+++ images360/items.py
@@ -0,0 +1,21 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy import Field, Item
+
+
+class Images360Item(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+
+class ImagesItem(Item):
+    collection = table = 'images'
+    id = Field()
+    url = Field()
+    title = Field()
+    thumb = Field()
